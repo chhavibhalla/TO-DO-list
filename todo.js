ac690d15024f4f103507a8c47fb2288c9e38b1cc
chhavibhalla <bhallachhavi007@gmail.com>
2025-02-10 14:46:12 +0530

    n
    n

--- todo.js
+++ todo.js
@@ -1,4 +1,17 @@
-function addtask(){
-    const task = document.getElementById("taskInput")
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let taskText = input.value.trim();
+    if (taskText === "") return;
     
+    let taskList = document.getElementById("taskList");
+    let li = document.createElement("li");
+    li.innerHTML = `${taskText} <button class='delete' onclick='removeTask(this)'>X</button>`;
+    taskList.appendChild(li);
+    
+    input.value = "";
+}
+
+function removeTask(button) {
+    let li = button.parentElement;
+    li.remove();
 }
